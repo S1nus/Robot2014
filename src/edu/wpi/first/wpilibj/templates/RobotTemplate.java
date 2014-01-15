7e266e487792114c3d2e3b53ef4f397c7b4067b0
S1nus <chuckhead7@gmail.com>
2014-01-15 15:19:22 -0500

    Added comments
    comments

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,33 +1,36 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class RobotTemplate extends SimpleRobot {
+    //Variable declaration zone
+    
+    //Controls
     Joystick leftstick;
     Joystick rightstick;
+    //Motors and drive train
     RobotDrive drive;
     public void robotInit() {
         System.out.println("Robot Initial");
+        
+        //Initializing our variables
+        
+        //Controls
         leftstick = new Joystick(1);
         rightstick = new Joystick(2);
+        //Motors and drive train
         drive = new RobotDrive(1,2,3,4);
-        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true); //These motors misbehaved, so we flipped them
         drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
     }
     public void autonomous() {
-        
+        //Autonomous code goes here, nothing yet.
     }
     public void operatorControl() {
         System.out.println("Entering Teleop");
         while(isOperatorControl() && isEnabled()) {
+            //Setting the left and rights sticks to control the drive train like a tank
             drive.tankDrive(leftstick, rightstick);
         }
     }
