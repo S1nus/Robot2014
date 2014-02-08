6646587d4939b0e1c81a92882ea1e803ba03a7c4
S1nus <chuckhead7@gmail.com>
2014-02-08 15:09:41 -0500

    Feeler Toggle
    Added controls for the feelers.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -11,8 +11,11 @@ public class RobotTemplate extends SimpleRobot {
     Joystick rightstick;
     //Motors and drive train
     RobotDrive drive;
+    Jaguar feeler1;
+    Jaguar feeler2;
+    double feelerspeed;
     public void robotInit() {
-        System.out.println("Robot Initial");
+        System.out.println("Robot Initialization");
         
         //Initializing our variables
         
@@ -20,6 +23,8 @@ public class RobotTemplate extends SimpleRobot {
         leftstick = new Joystick(1);
         rightstick = new Joystick(2);
         //Motors and drive train
+        feeler1 = new Jaguar(0); //Feeler1 - fix placeholder when completed
+        feeler2 = new Jaguar(0); //Feeler2 - fix placeholder when completed
         drive = new RobotDrive(1,2,3,4);
         drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true); //These motors misbehaved, so we flipped them
         drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
@@ -32,6 +37,26 @@ public class RobotTemplate extends SimpleRobot {
         while(isOperatorControl() && isEnabled()) {
             //Setting the left and rights sticks to control the drive train like a tank
             drive.tankDrive(leftstick, rightstick);
+            if (rightstick.getRawButton(3)) {
+                feeler1.set(feelerspeed);
+                feeler2.set(feelerspeed);
+            }
+            else {
+                feeler1.set(0.0);
+                feeler2.set(0.0);
+            }
+            if (rightstick.getRawButton(2)) {
+                toggleFeelers();
+            }
         }
+    }
+    public void toggleFeelers() {
+        if (feelerspeed == 1.0) {
+            feelerspeed = -1.0;
+        }
+        else if (feelerspeed == -1.0) {
+            feelerspeed = 1.0;
+        }
+        Timer.delay(1.5);
     }
 }
