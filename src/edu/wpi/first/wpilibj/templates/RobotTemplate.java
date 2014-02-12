ed9e615645f14b1cb46ff68bd07170f3de7de5ad
S1nus <chuckhead7@gmail.com>
2014-02-12 14:53:50 -0500

    Newest version
    Everything is good, the variable feeler speeds haven't been tested
    though.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,16 +1,17 @@
 package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.DriverStationLCD;
 
 public class RobotTemplate extends SimpleRobot {
     //Variable declaration zone
     //Controls
     Joystick leftstick;
     Joystick rightstick;
+    DriverStationLCD display = DriverStationLCD.getInstance();
     //Motors and drive train
     RobotDrive drive;
     Jaguar feeler1;
     Jaguar feeler2;
-    double feelerspeed;
     //Pneumatics
     DoubleSolenoid ds1;
     DoubleSolenoid ds2;
@@ -21,14 +22,19 @@ public class RobotTemplate extends SimpleRobot {
         //Controls
         leftstick = new Joystick(1);
         rightstick = new Joystick(2);
-        //Motors and drive train (placeholders)
-        feeler1 = new Jaguar(0);
-        feeler2 = new Jaguar(0);
-        drive = new RobotDrive(1,2,3,4); //Note: some motors may need to be flipped
-        //Pneumatics (placeholders)
-        ds1 = new DoubleSolenoid(0,0);
-        ds2 = new DoubleSolenoid(0,0);
-        compressor = new Compressor(0,0);
+        //Motors and drive train
+        feeler1 = new Jaguar(5);
+        feeler2 = new Jaguar(6);
+        drive = new RobotDrive(1,2,3,4);
+        //Because WPILib is kill
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+        drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        //Pneumatics
+        ds1 = new DoubleSolenoid(2,1);
+        ds2 = new DoubleSolenoid(4,3);
+        ds1.set(DoubleSolenoid.Value.kReverse);
+        ds2.set(DoubleSolenoid.Value.kReverse);
+        compressor = new Compressor(2,2);
     }
     
     public void autonomous() {
@@ -38,32 +44,54 @@ public class RobotTemplate extends SimpleRobot {
     }
     
     public void operatorControl() {
+        compressor.start();
         System.out.println("Entering Teleop");
         while(isOperatorControl() && isEnabled()) {
             //Setting the left and rights sticks to control the drive train like a tank
-            drive.tankDrive(leftstick, rightstick);
+            drive.tankDrive(rightstick, leftstick);
             if (rightstick.getRawButton(1)) {
-                fire();
             }
+            //Feelers
             if (leftstick.getRawButton(1)) {
-                feeler1.set(1.0);
-                feeler2.set(1.0);
-            }
+                feeler1.set(leftstick.getZ()*-1);
+                feeler2.set(leftstick.getZ());
+            } 
             else {
                 feeler1.set(0.0);
                 feeler2.set(0.0);
             }
         }
     }
-    
     public void fire() {
         ds1.set(DoubleSolenoid.Value.kForward);
         ds2.set(DoubleSolenoid.Value.kForward);
-        Timer.delay(1);
+        Timer.delay(0.1);
         ds1.set(DoubleSolenoid.Value.kOff);
         ds2.set(DoubleSolenoid.Value.kOff);
         Timer.delay(1);
-        ds1.set(DoubleSolenoid.Value.kReverse);
-        ds2.set(DoubleSolenoid.Value.kReverse);
+        //ds1.set(DoubleSolenoid.Value.kReverse);
+        //ds2.set(DoubleSolenoid.Value.kReverse);
+    }
+    
+    public void printDs() {
+        if (ds1.get() == DoubleSolenoid.Value.kForward) {
+            display.println(DriverStationLCD.Line.kUser1, 1, "ds1: forward");
+        }
+        else if (ds1.get() == DoubleSolenoid.Value.kReverse) {
+            display.println(DriverStationLCD.Line.kUser1, 1, "ds1: reverse");
+        }
+        else if (ds1.get() == DoubleSolenoid.Value.kOff) {
+            display.println(DriverStationLCD.Line.kUser1, 1, "ds1: off");
+        }
+        if (ds2.get() == DoubleSolenoid.Value.kForward) {
+            display.println(DriverStationLCD.Line.kUser1, 2, "ds2: forward");
+        }
+        else if (ds2.get() == DoubleSolenoid.Value.kReverse) {
+            display.println(DriverStationLCD.Line.kUser1, 2, "ds2: reverse");
+        }
+        else if (ds2.get() == DoubleSolenoid.Value.kOff) {
+            display.println(DriverStationLCD.Line.kUser1, 2, "ds2: off");
+        }        
+        display.updateLCD();
     }
 }
