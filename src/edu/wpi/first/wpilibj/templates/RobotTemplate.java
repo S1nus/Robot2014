b36e27cf7efd91e4f65cc6a7310a648c3f4e3127
S1nus <chuckhead7@gmail.com>
2014-02-18 18:11:06 -0500

    Newest version
    Updates, newest version

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,9 +1,12 @@
 package edu.wpi.first.wpilibj.templates;
-import edu.wpi.first.wpilibj.*;                                                                                                                 
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotTemplate extends SimpleRobot {
     //Variable declaration zone
     //Controls
+
     Joystick leftstick;
     Joystick rightstick;
     DriverStationLCD display = DriverStationLCD.getInstance();
@@ -11,16 +14,15 @@ public class RobotTemplate extends SimpleRobot {
     RobotDrive drive;
     Jaguar feeler1;
     Jaguar feeler2;
-    Encoder encoder1;
-    Encoder encoder2;
     //Pneumatics
     DoubleSolenoid ds1;
     DoubleSolenoid ds2;
+    Solenoid feelerSolenoid;
     Compressor compressor;
-    AnalogChannel pressureSensor;
-    int analog;
-    double pressure;
+    AnalogChannel pressureTransducer;
+    double analog;
     double psi;
+
     public void robotInit() {
         System.out.println("Robot Initialization");
         //Initializing our variables
@@ -30,32 +32,31 @@ public class RobotTemplate extends SimpleRobot {
         //Motors and drive train
         feeler1 = new Jaguar(5);
         feeler2 = new Jaguar(6);
-        drive = new RobotDrive(1,2,3,4);
-        //Because WPILib is kill
-        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-        drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        drive = new RobotDrive(1, 2, 3, 4);
         //Pneumatics
-        ds1 = new DoubleSolenoid(2,1);
-        ds2 = new DoubleSolenoid(4,3);
+        ds1 = new DoubleSolenoid(1, 2);
+        ds2 = new DoubleSolenoid(3, 4);
         ds1.set(DoubleSolenoid.Value.kReverse);
         ds2.set(DoubleSolenoid.Value.kReverse);
-        compressor = new Compressor(2,2);
-        pressureSensor = new AnalogChannel(2);
+        feelerSolenoid = new Solenoid(5);
+        compressor = new Compressor(2, 2);
+        pressureTransducer = new AnalogChannel(2);
     }
-    
+
     public void autonomous() {
         drive.drive(0.5, 0.0);
-        Timer.delay(1);
+        Timer.delay(3);
         drive.drive(0.0, 0.0);
+        fire();
     }
-    
+
     public void operatorControl() {
         compressor.start();
         System.out.println("Entering Teleop");
-        while(isOperatorControl() && isEnabled()) {
+        while (isOperatorControl() && isEnabled()) {
             //Setting the left and rights sticks to control the drive train like a tank
             drive.tankDrive(rightstick, leftstick);
-            drive.setSensitivity((rightstick.getZ()+1.0)*.5);
+            drive.setSensitivity((rightstick.getZ() + 1.0) * .5);
             if (rightstick.getRawButton(1)) {
                 fire();
             }
@@ -63,15 +64,21 @@ public class RobotTemplate extends SimpleRobot {
             if (leftstick.getRawButton(1)) {
                 feeler1.set(leftstick.getZ());
                 feeler2.set(leftstick.getZ());
-            } 
-            else {
+            } else {
                 feeler1.set(0.0);
                 feeler2.set(0.0);
+            }
+            if (rightstick.getRawButton(2)) {
+                feelerSolenoid.set(false);
+            }
+            if (rightstick.getRawButton(3)) {
+                feelerSolenoid.set(true);
             }
             getPressure();
             printDs();
         }
     }
+
     public void fire() {
         ds1.set(DoubleSolenoid.Value.kForward);
         ds2.set(DoubleSolenoid.Value.kForward);
@@ -82,36 +89,17 @@ public class RobotTemplate extends SimpleRobot {
         ds1.set(DoubleSolenoid.Value.kReverse);
         ds2.set(DoubleSolenoid.Value.kReverse);
     }
-    
+
     public void printDs() {
-        display.println(DriverStationLCD.Line.kUser1, 1, "PSI: "+ psi + "00");
-        
-        //Here we debug the pistons
-        if (ds1.get() == DoubleSolenoid.Value.kForward) {
-            display.println(DriverStationLCD.Line.kUser2, 1, "ds1: forward");
-        }
-        else if (ds1.get() == DoubleSolenoid.Value.kReverse) {
-            display.println(DriverStationLCD.Line.kUser2, 1, "ds1: reverse");
-        }
-        else if (ds1.get() == DoubleSolenoid.Value.kOff) {
-            display.println(DriverStationLCD.Line.kUser2, 1, "ds1: off");
-        }
-        if (ds2.get() == DoubleSolenoid.Value.kForward) {
-            display.println(DriverStationLCD.Line.kUser3, 1, "ds2: forward");
-        }
-        else if (ds2.get() == DoubleSolenoid.Value.kReverse) {
-            display.println(DriverStationLCD.Line.kUser3, 1, "ds2: reverse");
-        }
-        else if (ds2.get() == DoubleSolenoid.Value.kOff) {
-            display.println(DriverStationLCD.Line.kUser3, 1, "ds2: off");
-        }
+        display.println(DriverStationLCD.Line.kUser1, 1, "PSI: " + psi + "00");
+        display.println(DriverStationLCD.Line.kUser2, 1, "Feeler Sensitivity: " + leftstick.getZ());
+        display.println(DriverStationLCD.Line.kUser3, 1, "Voltage: " + analog);
         display.updateLCD();
     }
+
     public void getPressure() {
-            //gross linear function
-        analog = pressureSensor.getValue();
-        psi = ((.00001 * analog * analog) + (0.2442 * analog) - 23.254);
-        psi= (int) (psi+.5);
+        analog = pressureTransducer.getVoltage();
+        psi = (analog / 0.5) * 14.5;//Much better function
+        psi = (int) (psi + .5);
     }
-    
 }
