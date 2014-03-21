b4470820418c653adb4089cf62a1335d69d59c17
S1nus <chuckhead7@gmail.com>
2014-03-21 19:20:01 -0400

    Newest version
    Yet to be tested

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class RobotTemplate extends SimpleRobot {
     final boolean FEELERS_EXTEND = true;
     final boolean FEELERS_RETRACT = false;
+    final boolean CATAPULT_UP = true;
+    final boolean CATAPULT_DOWN = false;
     final long ONE_SECOND_IN_US = 1000000;
     //Controls
     Joystick leftstick;
@@ -40,10 +42,10 @@ public class RobotTemplate extends SimpleRobot {
         s2 = new Solenoid(2);
         s3 = new Solenoid(3);
         s4 = new Solenoid(4);
-        s1.set(false);
-        s2.set(false);
-        s3.set(false);
-        s4.set(false);
+        s1.set(CATAPULT_UP);
+        s2.set(CATAPULT_UP);
+        s3.set(CATAPULT_UP);
+        s4.set(CATAPULT_UP);
         feelerSolenoid = new Solenoid(5);
         compressor = new Compressor(2, 2);
         pressureTransducer = new AnalogChannel(3);
@@ -52,6 +54,11 @@ public class RobotTemplate extends SimpleRobot {
     public void autonomous() {
         compressor.start();
         feelerSolenoid.set(FEELERS_EXTEND);
+        Timer.delay(1.0);
+        s1.set(CATAPULT_DOWN);
+        s2.set(CATAPULT_DOWN);
+        s3.set(CATAPULT_DOWN);
+        s4.set(CATAPULT_DOWN);
         Timer.delay(1.0);
         long startTime = Timer.getUsClock();
         for (long currentTime = Timer.getUsClock(); (currentTime - startTime) <= (2*ONE_SECOND_IN_US); currentTime = Timer.getUsClock()) {
@@ -73,6 +80,9 @@ public class RobotTemplate extends SimpleRobot {
         while (isOperatorControl() && isEnabled()) {
             //Setting the left and rights sticks to control the drive train like a tank
             drive.tankDrive(rightstick, leftstick);
+            if (leftstick.getRawButton(3)) {
+                flip();
+            }
             //Feelers
             if (leftstick.getRawButton(1)) {
                 feeler1.set(leftstick.getZ());
@@ -86,10 +96,10 @@ public class RobotTemplate extends SimpleRobot {
                 fire();    
             }
             if (rightstick.getRawButton(3)) {
-                feelerSolenoid.set(false);
+                feelerSolenoid.set(FEELERS_EXTEND);
             }
             if (rightstick.getRawButton(2)) {
-                feelerSolenoid.set(true);
+                feelerSolenoid.set(FEELERS_RETRACT);
             }
             getPressure();
             printDs();
@@ -97,15 +107,15 @@ public class RobotTemplate extends SimpleRobot {
     }
 
     public void fire() {
-        s1.set(true);
-        s2.set(true);
-        s3.set(true);
-        s4.set(true);
+        s1.set(CATAPULT_UP);
+        s2.set(CATAPULT_UP);
+        s3.set(CATAPULT_UP);
+        s4.set(CATAPULT_UP);
         Timer.delay(.5);
-        s1.set(false);
-        s2.set(false);
-        s3.set(false);
-        s4.set(false);
+        s1.set(CATAPULT_DOWN);
+        s2.set(CATAPULT_DOWN);
+        s3.set(CATAPULT_DOWN);
+        s4.set(CATAPULT_DOWN);
     }
 
     public void printDs() {
