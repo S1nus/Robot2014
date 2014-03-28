975b609ac25200cf399d3ae17a5c0fcc4017b676
S1nus <chuckhead7@gmail.com>
2014-03-28 08:27:39 -0400

    Newest version
    Simplified, smartdashboard pressure gauge (if anyone is reading this,
    how do you change the units?), and a lock on the fire method that
    prevents it from firing if the beams are up.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -2,23 +2,23 @@ package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 public class RobotTemplate extends SimpleRobot {
     final boolean FEELERS_EXTEND = true;
     final boolean FEELERS_RETRACT = false;
-    final boolean CATAPULT_UP = true;
-    final boolean CATAPULT_DOWN = false;
+    final boolean CATAPULT_UP = false;
+    final boolean CATAPULT_DOWN = true;
     final long ONE_SECOND_IN_US = 1000000;
     //Controls
     Joystick leftstick;
     Joystick rightstick;
     DriverStationLCD display = DriverStationLCD.getInstance();
-    boolean flip = false;
     
     //Motors and drive train
     RobotDrive drive;
     Jaguar feeler1;
     Jaguar feeler2;
+    Encoder leftEncoder;
+    Encoder rightEncoder;
     
     //Pneumatics
     Solenoid s1, s2, s3, s4, feelerSolenoid;
@@ -37,56 +37,52 @@ public class RobotTemplate extends SimpleRobot {
         feeler1 = new Jaguar(5);
         feeler2 = new Jaguar(6);
         drive = new RobotDrive(1, 2, 3, 4);
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
         //Pneumatics
         s1 = new Solenoid(1);
         s2 = new Solenoid(2);
         s3 = new Solenoid(3);
         s4 = new Solenoid(4);
-        s1.set(CATAPULT_UP);
-        s2.set(CATAPULT_UP);
-        s3.set(CATAPULT_UP);
-        s4.set(CATAPULT_UP);
+        setCatapult(CATAPULT_DOWN);
         feelerSolenoid = new Solenoid(5);
         compressor = new Compressor(2, 2);
         pressureTransducer = new AnalogChannel(3);
     }
-
+    
     public void autonomous() {
         compressor.start();
         feelerSolenoid.set(FEELERS_EXTEND);
         Timer.delay(1.0);
-        s1.set(CATAPULT_DOWN);
-        s2.set(CATAPULT_DOWN);
-        s3.set(CATAPULT_DOWN);
-        s4.set(CATAPULT_DOWN);
+        setCatapult(CATAPULT_DOWN);
         Timer.delay(1.0);
         long startTime = Timer.getUsClock();
         for (long currentTime = Timer.getUsClock(); (currentTime - startTime) <= (2*ONE_SECOND_IN_US); currentTime = Timer.getUsClock()) {
-            drive.drive(((currentTime-startTime)/(ONE_SECOND_IN_US)*2), 0.0);
+            drive.drive(-((currentTime-startTime)/(ONE_SECOND_IN_US)*4), 0.0);
         }
         for (long currentTime = Timer.getUsClock(); (currentTime - startTime) <= (2*ONE_SECOND_IN_US); currentTime = Timer.getUsClock()) {
-            drive.drive(0.5, 0.0);
+            drive.drive(-0.1, 0.0);
         }
         for (long currentTime = Timer.getUsClock(); (currentTime - startTime) <= (2*ONE_SECOND_IN_US); currentTime = Timer.getUsClock()) {
-            drive.drive(1-((currentTime-startTime)/(ONE_SECOND_IN_US)*2), 0.0);
+            drive.drive(-(1-((currentTime-startTime)/(ONE_SECOND_IN_US)*4)), 0.0);
         }
         fire();
         Timer.delay(1);
-        drive.drive(1.0, 0.0);
     }
 
     public void operatorControl() {
         compressor.start();
+        
         while (isOperatorControl() && isEnabled()) {
             //Setting the left and rights sticks to control the drive train like a tank
-            drive.tankDrive(rightstick, leftstick);
-            if (leftstick.getRawButton(3)) {
-                flip();
-            }
+            drive.tankDrive(leftstick, rightstick);
             //Feelers
             if (leftstick.getRawButton(1)) {
-                feeler1.set(leftstick.getZ());
-                feeler2.set(leftstick.getZ());
+                //feeler1.set(leftstick.getZ());
+                //feeler2.set(leftstick.getZ());
+                minifire();
             }
             else {
                 feeler1.set(0.0);
@@ -107,15 +103,17 @@ public class RobotTemplate extends SimpleRobot {
     }
 
     public void fire() {
-        s1.set(CATAPULT_UP);
-        s2.set(CATAPULT_UP);
-        s3.set(CATAPULT_UP);
-        s4.set(CATAPULT_UP);
-        Timer.delay(.5);
-        s1.set(CATAPULT_DOWN);
-        s2.set(CATAPULT_DOWN);
-        s3.set(CATAPULT_DOWN);
-        s4.set(CATAPULT_DOWN);
+        if (beamsDown()) {
+            setCatapult(CATAPULT_UP);
+            Timer.delay(.5);
+            setCatapult(CATAPULT_DOWN);
+        }
+    }
+    
+    public void minifire() {
+        setCatapult(CATAPULT_UP);
+        Timer.delay(.01);
+        setCatapult(CATAPULT_DOWN);
     }
 
     public void printDs() {
@@ -123,27 +121,29 @@ public class RobotTemplate extends SimpleRobot {
         display.println(DriverStationLCD.Line.kUser2, 1, "Feeler Sensitivity: " + leftstick.getZ());
         display.println(DriverStationLCD.Line.kUser3, 1, "Voltage: " + analog);
         display.updateLCD();
+        SmartDashboard.putNumber("pressure", psi);
     }
 
     public void getPressure() {
         analog = pressureTransducer.getVoltage();
-        psi = (analog / 0.5) * 14.5;//Much better function
-        psi = (int) (psi + .5);
+        psi = ((51.4166331*analog)-24.97030688);
+    } 
+        
+    public void setCatapult(boolean mode) {
+        s1.set(mode);
+        s2.set(mode);
+        s3.set(mode);
+        s4.set(mode);
     }
-    
-    public void flip() {
-        long currentTime = Timer.getUsClock();
-        long flipUsTime = 0;
-        if ((currentTime - flipUsTime) >= 125000) {
-            flipUsTime = currentTime;
-            if (flip) {
-                flip = false;
-                drive.setSensitivity(0.5);
-            }
-            else {
-                flip = true;
-                drive.setSensitivity(-0.5);
-            }
-        }
+    public boolean beamsDown() {
+        return feelerSolenoid.get();
+    }
+    public void disabled() {
+        feelerSolenoid.set(FEELERS_EXTEND);
+        Timer.delay(0.5);
+        setCatapult(CATAPULT_DOWN);
+        Timer.delay(0.5);
+        feelerSolenoid.set(FEELERS_RETRACT);
+        Timer.delay(0.5);
     }
 }
